dc412c47cc88756ddffb12d1ecf58376f4da785d
Akib Husain <akibhusain830@gmail.com>
2025-11-11 17:01:47 +0530

    Add TypeScript module declarations for image files (png, jpg, avif, etc)

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -3,3 +3,33 @@ declare module '*.svg' {
   const content: FC<SVGProps<SVGSVGElement>>;
   export default content;
 }
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.avif' {
+  const content: string;
+  export default content;
+}
